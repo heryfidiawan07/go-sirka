ac8af8597b2575eef96fe4b3f02d7517b9cd3b3a
Hery Fidiawan <heryfidiawan07@gmail.com>
2022-07-09 13:22:22 +0700

    Update Dockerfile docker-compose & config/db

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,38 @@
-FROM golang:1.14.9-alpine
-RUN mkdir /build
-ADD go.mod go.sum server.go /build/
-WORKDIR /build
-RUN go build
+# FROM golang:1.14.9-alpine
+# RUN mkdir /build
+# ADD go.mod go.sum server.go /build/
+# WORKDIR /build
+# RUN go build
+
+# Start from golang base image
+FROM golang:alpine
+
+# Add Maintainer info
+LABEL maintainer="Heri Fidiawan"
+
+# Install git.
+# Git is required for fetching the dependencies.
+# RUN apk update && apk add --no-cache git && apk add --no-cach bash && apk add build-base
+
+# Setup folders
+RUN mkdir /app
+WORKDIR /app
+
+# Copy the source from the current directory to the working Directory inside the container
+COPY . .
+COPY .env .
+
+# Download all the dependencies
+RUN go get -d -v ./...
+
+# Install the package
+RUN go install -v ./...
+
+# Build the Go app
+RUN go build -o /build
+
+# Expose port 8080 to the outside world
+EXPOSE 8081
+
+# Run the executable
+CMD [ "/build" ]
--- config/db.go
+++ config/db.go
@@ -3,7 +3,9 @@ package config
 import (
 	"app/models"
 	"fmt"
+	"os"
 
+	"github.com/subosito/gotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,7 +13,10 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=ec2-34-239-241-121.compute-1.amazonaws.com user=gcbuxphbdrheuy password=9fbe7f4aad460c3b93ca1cb97fee21dfd4504c5059475940d06f39baa9cc1b51 dbname=dcvndkl01j4frj port=5432 sslmode=require TimeZone=Asia/Jakarta"
+	gotenv.Load()
+
+	// dsn := "host=ec2-34-239-241-121.compute-1.amazonaws.com user=gcbuxphbdrheuy password=9fbe7f4aad460c3b93ca1cb97fee21dfd4504c5059475940d06f39baa9cc1b51 dbname=dcvndkl01j4frj port=5432 sslmode=require TimeZone=Asia/Jakarta"
+	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=require TimeZone=Asia/Jakarta"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
