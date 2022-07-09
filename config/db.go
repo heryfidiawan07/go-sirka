e5b24c4166be0a7e5f039a8a14ef176cad5cbc9b
Hery Fidiawan <heryfidiawan07@gmail.com>
2022-07-09 10:46:24 +0700

    Update DB crendentials heroku

--- config/db.go
+++ config/db.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=localhost user=postgres password=root dbname=sirka_rest_api port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := "host=ec2-34-239-241-121.compute-1.amazonaws.com user=gcbuxphbdrheuy password=9fbe7f4aad460c3b93ca1cb97fee21dfd4504c5059475940d06f39baa9cc1b51 dbname=dcvndkl01j4frj port=5432 sslmode=require TimeZone=Asia/Jakarta"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
